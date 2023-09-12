a0e8c181745a02c67cd7a740271f770a63261f10
Ricardo Alexandre <49127332+VespertineWeb@users.noreply.github.com>
2023-09-12 12:01:34 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -29,7 +29,7 @@ EOF
 #######################################
 system_git_clone() {
   print_banner
-  printf "${WHITE} 💻 Fazendo download do código B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Fazendo download do código...${GRAY_LIGHT}"
   printf "\n\n"
 
 
@@ -49,7 +49,7 @@ EOF
 #######################################
 system_update() {
   print_banner
-  printf "${WHITE} 💻 Vamos atualizar o sistema B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos atualizar o sistema...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -71,7 +71,7 @@ EOF
 #######################################
 deletar_tudo() {
   print_banner
-  printf "${WHITE} 💻 Vamos deletar o B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos deletar o sistema selecionado...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -117,7 +117,7 @@ EOF
 #######################################
 configurar_bloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos bloquear o B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos bloquear o sistema selecionado...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -144,7 +144,7 @@ EOF
 #######################################
 configurar_desbloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Desbloquear o B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos desbloquear o sistema...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -170,7 +170,7 @@ EOF
 #######################################
 configurar_dominio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Alterar os Dominios do B2Z...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos alterar os dominios...${GRAY_LIGHT}"
   printf "\n\n"
 
 sleep 2
@@ -275,7 +275,7 @@ EOF
 #######################################
 system_node_install() {
   print_banner
-  printf "${WHITE} 💻 Instalando nodejs...${GRAY_LIGHT}"
+  printf "${WHITE} 📗 Instalando nodejs...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -303,7 +303,7 @@ EOF
 #######################################
 system_docker_install() {
   print_banner
-  printf "${WHITE} 💻 Instalando docker...${GRAY_LIGHT}"
+  printf "${WHITE} 🐋 Instalando docker...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
